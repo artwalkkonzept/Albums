301473d01cc5a5d6072ce88c10698b2f1ba313dd
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-10-29 10:45:34 +0200

    First mini deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const appName = "Photo Gallery API"; 
 const port = process.env.PORT || 8080;
-const createServer = require("./server");
+const createServer = require("./src/server");
 const server = createServer({
   photosDb: "photos.db",
   albumsDb: "albums.db"
